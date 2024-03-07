dc5180862830fe696bccbf407793e79df5e26072
leorega <leonardoregazzoni@gmail.com>
2024-03-07 16:38:35 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+const App = () => {
+    return (
+        <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
+            <div className="bg-gray-950 p-4 w-1/5">
+                <h1 className="text-3xl font-bold text-center block my2">
+                    Tasks App
+                </h1>
+                <TaskForm />
+                <TaskList />
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+const TaskForm = () => {
+    const [task, setTask] = useState({
+        title: "",
+        description: "",
+        done: false,
+    });
+
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => setTask({ ...task, [e.target.name]: e.target.value });
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(task);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+                    placeholder="Write a title"
+                    onChange={handleChange}
+                />
+                <textarea
+                    name="description"
+                    rows={3}
+                    className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2"
+                    placeholder="Write a description"
+                    onChange={handleChange}
+                ></textarea>
+                <label htmlFor="" className="inline-flex items-center gap-x-2">
+                    <input
+                        type="checkbox"
+                        className="h-5 w-5 text-indigo-600"
+                    />
+                    <span>Done</span>
+                </label>
+                <button className="bg-indigo-500 px-3 block py-2 w-full">
+                    Save
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default TaskForm;
